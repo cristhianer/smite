86ef48fe5b2d5d711ca92abd0a5ac4267f6f1cc7
CristhianeToledo <resendecristhiane@gmail.com>
2017-03-26 19:43:48 -0300

    - Footer added and styled; - Validation of password and email completed;

---
+++ js/loginValidator.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+	console.log("oi");
+
+
+	$('#loginButton').click(function(){
+	    var email=$('#email').val();
+	    console.log(email);
+	    var emailReg = /^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+	    
+	    if( !emailReg.test( email ) ) {
+	        alert('Please enter valid email');
+	        $('#login-standard').hide();
+	        $('#login-error').show();
+	    } else {
+        	alert('Thank you for your valid email');
+	        $('#login-standard').hide();      
+	        $('#login-error').hide();	          	
+        	$('#login-confirmed').show();
+        }
+
+	    var password=$('#password').val();
+	    console.log(password);
+	    var passwordReg = /((?=.*\d)(?=.*[a-z])(?=.*[@#$%]).{6,20})/;
+	    
+	    if( !passwordReg.test(password) ) {
+	        alert('Please enter valid password');
+	        $('#password-standard').hide();
+	        $('#password-error').show();
+	    } else {
+        	alert('Thank you for your valid password');
+            $('#password-standard').hide();        	
+ 	        $('#password-error').hide();           
+        	$('#password-confirmed').show();
+        }
+
+
+
+
+
+
+        
+	});
+	  
+
+});
